8538ba6017c4bd0de02024abea822de8551b9117
sengoku-f <honoka.github@outlook.com>
2019-12-12 14:57:45 +0800

    :bookmark: v 1.1

--- Bat批处理/2-Gif压缩.bat
+++ Bat批处理/2-Gif压缩.bat
@@ -1,6 +1,9 @@
+:: Gif压缩 720P v1.1
+:: 添加自定义操作
+:: 修改匹配符(*.gif)为(%*),匹配所选多个文件
 cd "%~dp1"
 set size=1200
 set /p size=resize:
-for %%a in (*.gif) do (
+for %%a in (%*) do (
 gifsicle --resize-fit-height %size% -O3 --lossy=80 --colors 256 "%%~fa" -o "%%~na_%size%_lossy80.gif"
 )
