9a2fd51b95a01b49aefbf8c2e38a6573a2dd6b06
sengoku-f <honoka.github@outlook.com>
2020-07-01 14:19:40 +0800

    添加同步路径

--- Bat批处理/3-Rclone-OneDrive.bat
+++ Bat批处理/3-Rclone-OneDrive.bat
@@ -1,4 +1,4 @@
-@echo off & title Rclone 同步 SendTo → onedrive By Sengoku v1.1
+@echo off & title Rclone 同步 SendTo → onedrive By Sengoku v1.2
 REM 设置代理
 set http_proxy=socks5://127.0.0.1:1080
 set https_proxy=socks5://127.0.0.1:1080
@@ -6,6 +6,9 @@ set http_proxy=http://127.0.0.1:1081
 set https_proxy=http://127.0.0.1:1081
 REM 开始同步 SendTo
 rclone sync "C:\Users\Administrator\AppData\Roaming\Microsoft\Windows\SendTo" onedrive:"自动化\Windows 脚本\SendTo" -P --log-file="C:\Users\Administrator\Rclone_win.log" --include *.{bat}
+
+REM 开始同步 Win-Automator
+rclone sync "D:\Git\Win-Automator" onedrive:"自动化\Windows 脚本" -P --log-file="C:\Users\Administrator\Rclone_win.log" --exclude ".git/" --exclude "SendTo/" --exclude "*.md"
 
 REM 复制 results
 rclone copy onedrive:"Windows 软件\stairspeedtest_reborn_win64\results" onedrive:"科学上网\测速图" -P --log-file="C:\Users\Administrator\Rclone_win.log" --include *.{png,jpg}
