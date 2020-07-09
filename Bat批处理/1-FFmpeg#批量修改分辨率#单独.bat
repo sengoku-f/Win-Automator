3bc5bf551dde0342e8b638fe105384d986f16f31
sengoku-f <honoka.github@outlook.com>
2020-07-09 13:23:22 +0800

    更新 [FFmpeg批量修改分辨率] v1.3

--- Bat批处理/1-FFmpeg#批量修改分辨率#单独.bat
+++ Bat批处理/1-FFmpeg#批量修改分辨率#单独.bat
@@ -1,7 +1,7 @@
-@echo off & title FFmpeg 视频修改分辨率 (单独) By Sengoku v1.2
+@echo off & title FFmpeg 视频修改分辨率 (单独) By Sengoku v1.3
+REM 添加手动输入分辨率
 cd "%~dp1"
 REM mkdir 改变分辨率
-set videoSIZE=5760:2460
-set videoSIZENAME=%videoSIZEREM=x%
-REM echo %VAT%
+set /p videoSIZENAME=输入视频尺寸,如1920x1080:
+set videoSIZE=%videoSIZENAME:x=:%
 ffmpeg -i "%~nx1" -c:v libx264 -crf 23 -vf scale=%videoSIZE%:flags=lanczos,setsar=1 -c:a copy -y "%~n1_%videoSIZENAME%.mp4"
--- Bat批处理/1-FFmpeg#批量修改分辨率.bat
+++ Bat批处理/1-FFmpeg#批量修改分辨率.bat
@@ -1,5 +1,8 @@
-@echo off & title FFmpeg 批量修改分辨率 By Sengoku v1.0
+@echo off & title FFmpeg 批量修改分辨率 By Sengoku v1.3
+REM 添加手动输入分辨率
 cd "%~dp1"
 REM mkdir 改变分辨率
-for %%a in (*.mp4;*.mov;*.flv;*.mkv;*.avi) do ffmpeg -i "%%~fa" -c:v libx264 -crf 23 -vf scale=iw/4:-2:flags=lanczos,setsar=1 -c:a copy -y "%%~na_修改分辨率.mp4"
+set /p videoSIZENAME=输入视频尺寸,如1920x1080:
+set videoSIZE=%videoSIZENAME:x=:%
+for %%a in (*.mp4;*.mov;*.flv;*.mkv;*.avi) do ffmpeg -i "%%~fa" -c:v libx264 -crf 23 -vf scale=%videoSIZE%:flags=lanczos,setsar=1 -c:a copy -y "%%~na_%videoSIZENAME%.mp4"
 
