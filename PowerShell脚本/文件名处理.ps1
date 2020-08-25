a41c2c12c8a857b430e5056d151951042ef1abb0
sengoku-f <honoka.github@outlook.com>
2020-08-25 10:47:51 +0800

    添加文件目录深度

--- PowerShell脚本/文件名处理.ps1
+++ PowerShell脚本/文件名处理.ps1
@@ -2,7 +2,16 @@ Get-ChildItem -Recurse -Force -Filter "* *" | Rename-Item -NewName { $_.Name -re
 Get-ChildItem -Recurse -Force -Filter "*–*" | Rename-Item -NewName { $_.Name -replace "–","-" }
 Get-ChildItem -Recurse -Force -Filter "*：*" | Rename-Item -NewName { $_.Name -replace "：","-" }
 
-#  cmd いㄏノ
+# 设定目录深度，和匹配文件
+
+Get-ChildItem -Recurse -Force -Filter "* *" -Depth 0 -Name -File -Include "*" | Rename-Item -NewName { $_.Name -replace " ","" }
+Get-ChildItem -Recurse -Force -Filter "*–*" -Depth 0 -Name -File -Include "*" | Rename-Item -NewName { $_.Name -replace "–","_" }
+Get-ChildItem -Recurse -Force -Filter "*-*" -Depth 0 -Name -File -Include "*" | Rename-Item -NewName { $_.Name -replace "-","_" }
+Get-ChildItem -Recurse -Force -Filter "*：*" -Depth 0 -Name -File -Include "*" | Rename-Item -NewName { $_.Name -replace "：","_" }
+Get-ChildItem -Recurse -Force -Filter "*（*" -Depth 0 -Name -File -Include "*" | Rename-Item -NewName { $_.Name -replace "（","(" }
+Get-ChildItem -Recurse -Force -Filter "*）*" -Depth 0 -Name -File -Include "*" | Rename-Item -NewName { $_.Name -replace "）",")" }
+
+# cmd 中使用
 
 powershell -command "Get-ChildItem -Recurse -Force -Filter '* *' | Rename-Item -NewName { $_.Name -replace ' ','' }"
 powershell -command "Get-ChildItem -Recurse -Force -Filter '*–*' | Rename-Item -NewName { $_.Name -replace '–','-' }"
