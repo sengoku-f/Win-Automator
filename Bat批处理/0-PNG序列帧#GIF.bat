6542a8d41555da814899617693aa7dbf2d880d37
sengoku-f <honoka.github@outlook.com>
2020-09-08 18:55:42 +0800

    更新 v1.1, 添加循环, 大小限制

--- Bat批处理/0-PNG序列帧#GIF.bat
+++ Bat批处理/0-PNG序列帧#GIF.bat
@@ -1,9 +1,20 @@
-@echo off & title 序列帧 → GIF By Sengoku v1.0
+@echo off & title 序列帧 → GIF By Sengoku v1.1
+REM 添加循环
+REM 使用图像读取修饰符[640x640]加速, -thumbnail 640x 也可以
+REM -------------------------------------------------
+REM 定位到父目录
 cd "%~dp1"
-magick -fuzz 1.6% -delay 4 -loop 0 "*.png" -layers OptimizePlus -layers OptimizeTransparency "out.gif"
-gifsicle -O3 --lossy=80 --colors 256 "out.gif" -o "out_o.gif"
-for /f "delims=" %%i in ("%cd%") do set folder=%%~ni
-echo %folder%
-if exist "%folder%.gif" del /q "%folder%.gif"
-ren out_o.gif "%folder%.gif"
-del /q out.gif
+REM 设置循环，0=无限循环
+set loop=0
+REM 开始循环
+for /d %%i in (%*) do (
+    cd "%%~i"
+    REM 执行转换
+    timecmd magick -fuzz 1.6%% -delay 4 -loop %loop% "*.png"[640x] -treedepth 4 -layers OptimizePlus -layers OptimizeTransparency "out.gif"
+    REM 压缩
+    timecmd gifsicle -O3 --lossy=80 --colors 256 --resize-touch 640x_ "out.gif" -o "%%~i.gif"
+
+    REM 删除
+    del /q "out.gif"
+)
+REM 循环结束
