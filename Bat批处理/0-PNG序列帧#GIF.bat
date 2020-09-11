745196f40dd4dfeb6f9fe73efefc7fa2910f2db5
sengoku-f <honoka.github@outlook.com>
2020-09-11 16:41:00 +0800

    添加帧数计算

--- Bat批处理/0-PNG序列帧#GIF.bat
+++ Bat批处理/0-PNG序列帧#GIF.bat
@@ -1,4 +1,5 @@
-@echo off & title 序列帧 → GIF By Sengoku v1.1
+@echo off & title 序列帧 → GIF By Sengoku v1.2
+REM 添加帧数计算
 REM 添加循环
 REM 使用图像读取修饰符[640x640]加速, -thumbnail 640x 也可以
 REM -------------------------------------------------
@@ -6,13 +7,16 @@ REM 定位到父目录
 cd "%~dp1"
 REM 设置循环，0=无限循环
 set loop=0
+REM 设置帧数
+set fps=25
+set /a delay=100/%fps%
 REM 开始循环
 for /d %%i in (%*) do (
     cd "%%~i"
     REM 执行转换
-    timecmd magick -fuzz 1.6%% -delay 4 -loop %loop% "*.png"[640x] -treedepth 4 -layers OptimizePlus -layers OptimizeTransparency "out.gif"
+    magick -fuzz 1.6%% -delay %delay% -loop %loop% "*.png"[640x] -treedepth 4 -layers OptimizePlus -layers OptimizeTransparency "out.gif"
     REM 压缩
-    timecmd gifsicle -O3 --lossy=80 --colors 256 --resize-touch 640x_ "out.gif" -o "%%~i.gif"
+    gifsicle -O3 --lossy=80 --colors 256 --resize-touch 640x_ "out.gif" -o "%%~i.gif"
 
     REM 删除
     del /q "out.gif"
